9bbb66f69ed210552dab702424736129b2e3cbfe
akashdobariya <akash2702dobariya@gmail.com>
2013-05-28 05:52:30 -0700

    akash Can

---
+++ src/java/com/cms/action/CanRegisterAction.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cms.action;
+
+import com.cms.model.canRegister;
+import com.opensymphony.xwork2.ModelDriven;
+
+
+/**
+ *
+ * @author akash
+ */
+public class CanRegisterAction implements ModelDriven{
+
+ canRegister can=new canRegister();
+    
+    @Override
+    public Object getModel() {
+        return can;
+    }
+    
+    public String docanRegister(){
+        boolean b;
+        b=can.cRegister();
+        if(b)
+        return "success";
+        else
+        return "failure";
+        
+    }
+    
+
+    
+    
+}
